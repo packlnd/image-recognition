4663f53df2b6174fee38bb9e97a511900de646ee
Patrik Ackland <patrikackland@gmail.com>
2016-09-05 12:27:46 -0700

    Started training code

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/png"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
 	// http://stats.stackexchange.com/questions/39037/how-does-neural-network-recognise-images
-	fmt.Println("Hello, world!")
+}
+
+func train() {
+	files, _ := ioutil.ReadDir("./images/good")
+	for _, f := range files {
+		infile, _ := os.Open(f)
+		src, _, _ := image.Decode(infile)
+		fmt.Println(src)
+	}
 }
