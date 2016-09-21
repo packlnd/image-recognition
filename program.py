b54e2129cb1ba30e1337b241bd78738db45382bd
Patrik Ackland <patrikackland@gmail.com>
2016-09-22 00:05:37 +0200

    Started working on simple log regr code

--- program.py
+++ program.py
@@ -6,3 +6,7 @@ print(sess.run(hello))
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
