4259556e00cc8c6e179f8bc611c1fb43691335ca
Patrik Ackland <patrikackland@gmail.com>
2016-11-11 18:43:22 +0100

    Now that tensorflow works, started working on implementation of image classifier@ '

--- program.py
+++ program.py
@@ -1,12 +1,22 @@
 import tensorflow as tf
 
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
-a = tf.constant(10)
-b = tf.constant(32)
-print(sess.run(a + b))
+# hello = tf.constant('Hello, TensorFlow!')
+# sess = tf.Session()
+# print(sess.run(hello))
+# a = tf.constant(10)
+# b = tf.constant(32)
+# print(sess.run(a + b))
 
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+# read image
+# https://research.googleblog.com/2016/03/train-your-own-image-classifier-with.html
+
+# x = tf.placeholder(tf.float32, [None, 784])
+# W = tf.Variable(tf.zeros([784, 10]))
+# b = tf.Variable(tf.zeros([10]))
+
+def img_to_feature_vector():
+
+def train():
+    raise NotImplementedError
+
+def test():
